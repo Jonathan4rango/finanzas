f07f38960e5fc778c4ab8f7ae2dd144949b2fac2
Jonathan Arley Arango Mejía <soportegeoambientalcarder@sigmaingenieria.net>
2023-11-28 07:25:34 -0500

    Primer commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+task = signal([
+'Primero',
+'Segundo']
+  )
+changeHandler(event: Event){
+  const input = event.target as HTMLInputElement;
+  const newTask = input.value;
+  this.task.update((task)=>[...task,newTask]);
+}
+
+deleteTask(index: number){
+this.task.update((task)=> task.filter((task, posistion) => posistion !== index));
+}
+}
